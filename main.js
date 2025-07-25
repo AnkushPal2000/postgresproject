0238c19e87c563633b9d432d252affc5a13fa066
AnkushPal2000 <ankushpal493@gmail.com>
2025-07-25 12:39:11 +0530

    cc

---
+++ main.js
@@ -0,0 +1,27 @@
+const { Client } = require("pg");
+
+const conn = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "1312",
+  database: "AnkushDB",
+});
+
+conn
+  .connect()
+  .then(() => {
+    console.log("Connected to the database successfully!");
+
+    conn.query("SELECT * FROM demotable", (err, res) => {
+      if (!err) {
+        console.log("Query results:", res.rows);
+      } else {
+        console.error("Query error:", err);
+      }
+      conn.end(); // Only end after query completes
+    });
+  })
+  .catch((err) => {
+    console.error("Connection error:", err.stack);
+  });
