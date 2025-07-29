ef678e2926a539dd8d02a2b66dfaa7341937d12c
AnkushPal2000 <ankushpal493@gmail.com>
2025-07-29 11:19:32 +0530

    cc

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const conn = new Client({
   user: "postgres",
   port: 5432,
   password: "mydatabase",
-  database: "ankushdb",
+  database: "postgres",
   // ssl: {
   //   rejectUnauthorized: false,
   // },
